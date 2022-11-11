968345695dfcb21f27b0e07a2647ef3dc04c6f1e
Чеботарь Игорь Евгеньевич <giborg@inbox.ru>
2022-11-11 19:39:27 +0300

    Balaboba api

---
+++ src/main/java/Balaboba.java
@@ -0,0 +1,45 @@
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+public class Balaboba {
+    public static String continueText(String query) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://yandex.ru/lab/api/yalm/text3";
+        return Objects.requireNonNull(
+                        restTemplate.postForObject(
+                                url,
+                                new Request(query),
+                                Response.class))
+                .getText();
+    }
+
+    private static class Request {
+        private String query;
+
+        public Request() {
+        }
+
+        public Request(String query) {
+            this.query = query;
+            int intro = 0;
+            int filter = 1;
+        }
+
+        public String getQuery() {
+            return query;
+        }
+
+        public void setQuery(String query) {
+            this.query = query;
+        }
+    }
+
+    private static class Response {
+        private String text;
+
+        public String getText() {
+            return text;
+        }
+    }
+}
